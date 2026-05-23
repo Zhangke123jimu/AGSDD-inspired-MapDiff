d5399362ddad04ae7c5bec7ee53ec1b40784a1ac
Ke Zhang <zhangke123jimu@163.com>
2026-05-23 19:17:17 +0800

    Enable distributed training and resuming from a checkpoint

--- mask_ipa_pretrain.py
+++ mask_ipa_pretrain.py
@@ -2,6 +2,7 @@ from comet_ml import Experiment
 import hydra
 import os
 import torch
+
 from dataloader.large_dataset import Cath
 from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
@@ -9,15 +10,35 @@ from dataloader.collator import CollatorIPAPretrain
 from model.ipa.ipa_net import IPANetPredictor
 from torch.optim import Adam, lr_scheduler
 from trainer.mask_ipa_trainer import Trainer
-
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+def setup_ddp():
+    distributed = "LOCAL_RANK" in os.environ
+
+    if distributed:
+        local_rank = int(os.environ["LOCAL_RANK"])
+        torch.cuda.set_device(local_rank)
+        dist.init_process_group(backend="nccl")
+        rank = dist.get_rank()
+        world_size = dist.get_world_size()
+    else:
+        local_rank = 0
+        rank = 0
+        world_size = 1
+
+    return distributed,local_rank, rank, world_size,rank==0
 
 @hydra.main(version_base=None, config_path="conf", config_name="mask_pretrain")
 def main(cfg: DictConfig):
-    if cfg.comet.use:
+    distributed, local_rank, rank, world_size,is_main_process = setup_ddp()
+    device = torch.device(f'cuda:{local_rank}' if torch.cuda.is_available() else 'cpu')
+
+    if cfg.comet.use and is_main_process:
         experiment = Experiment(
             project_name=cfg.comet.project_name,
             workspace=cfg.comet.workspace,
@@ -36,47 +57,72 @@ def main(cfg: DictConfig):
     else:
         experiment = None
     output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-    print(OmegaConf.to_yaml(cfg))
-    print(f"Output directory: {output_dir}")
-    if experiment:
+    if is_main_process:
+        print(OmegaConf.to_yaml(cfg))
+        print(f"Output directory: {output_dir}")
+    if experiment and is_main_process:
         experiment.log_parameters({"output_dir": output_dir})
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     train_ID = os.listdir(cfg.dataset.train_dir)
 
     train_dataset = Cath(train_ID, cfg.dataset.train_dir)
 
+    train_sampler = DistributedSampler(train_dataset,shuffle=True) if distributed else None
+
     collator = CollatorIPAPretrain(candi_rate=cfg.train.candi_rate, mask_rate=cfg.train.mask_rate,
                                    replace_rate=cfg.train.replace_rate, keep_rate=cfg.train.keep_rate)
 
-    train_loader = DataLoader(train_dataset, batch_size=cfg.train.batch_size, shuffle=True, num_workers=24,
+    train_loader = DataLoader(train_dataset, batch_size=cfg.train.batch_size, shuffle=(train_sampler is None), num_workers=12, sampler=train_sampler,
                               collate_fn=collator)
 
     model = IPANetPredictor(dropout=cfg.model.ipa_drop_out).to(device)
+    resume_checkpoint=None
+    if cfg.resume_path is not None:
+        try:
+            resume_checkpoint = torch.load(cfg.resume_path,map_location=device)
+            model.load_state_dict(resume_checkpoint['model'], strict=False)
+            if is_main_process:
+                print(f"Resuming from checkpoint {cfg.resume_path}: \n"
+                      f"  Global epoch: {resume_checkpoint['global_epoch']} \n"
+                      f"  Current train loss: {resume_checkpoint['loss']} \n" )
 
-    if torch.cuda.device_count() > 1:
-        print(f"Use {torch.cuda.device_count()} GPUs with DataParallel")
-        model = torch.nn.DataParallel(model)
+        except Exception as e:
+            raise RuntimeError(f"Failed to resume from {cfg.resume_path}") from e
 
-    print(f"Total parameters: {count_parameters(model)}")
+    if distributed:
+        model = DDP(model, device_ids=[local_rank], output_device=local_rank)
+
+    optimizer = Adam(model.parameters(), lr=cfg.train.lr, betas=(0.95, 0.999), weight_decay=cfg.train.weight_decay)
+    if cfg.resume_path is not None:
+        optimizer.load_state_dict(resume_checkpoint['opt'])
 
     steps_per_epoch = len(train_loader)
-    optimizer = Adam(model.parameters(), lr=cfg.train.lr, betas=(0.95, 0.999), weight_decay=cfg.train.weight_decay)
     if cfg.train.scheduler:
         scheduler = lr_scheduler.OneCycleLR(optimizer, max_lr=cfg.train.lr, total_steps=cfg.train.train_epochs * steps_per_epoch)
     else:
         scheduler = None
 
+    if cfg.resume_path is not None and scheduler is not None and resume_checkpoint["scheduler"] is not None:
+        scheduler.load_state_dict(resume_checkpoint["scheduler"])
+
+    if is_main_process:
+        print(f"Total parameters: {count_parameters(model)}")
+
     loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
 
     trainer = Trainer(config=cfg, model=model, optimizer=optimizer, epochs=cfg.train.train_epochs, loss_fn=loss_fn,
                       train_dataloader=train_loader, output_dir=output_dir, device=device,
-                      scheduler=scheduler, experiment=experiment)
-
+                      scheduler=scheduler, experiment=experiment, resume_path=cfg.resume_path,distributed=distributed,
+                      is_main_process=is_main_process,train_sampler=train_sampler)
     trainer.fit()
-    print(f"Output directory: {output_dir}")
 
-    os.system("auto_save_shutdown.sh")
+    if distributed:
+        dist.barrier()
+        dist.destroy_process_group()
+
+    if is_main_process:
+        print(f"Output directory: {output_dir}")
+        os.system("auto_save_shutdown.sh")
 
 if __name__ == "__main__":
     main()
--- trainer/mask_ipa_trainer.py
+++ trainer/mask_ipa_trainer.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 
 class Trainer(object):
     def __init__(self, config, model, optimizer, epochs, loss_fn, train_dataloader, output_dir, device,
-                 scheduler=None, experiment=None, save_epochs=50):
+                 scheduler=None, experiment=None, save_epochs=10, resume_path=None,distributed=False,is_main_process=True,train_sampler=None,):
         self.model = model
         self.config = config
         self.optimizer = optimizer
@@ -20,15 +20,21 @@ class Trainer(object):
         self.train_dataloader = train_dataloader
         self.loss_fn = loss_fn
         self.train_num_epochs = epochs
-        self.current_epoch = 0
         self.step = 0
         self.device = device
         self.output_dir = output_dir
         self.save_epochs = save_epochs
+        self.distributed = distributed
+        self.is_main_process = is_main_process
+        self.train_sampler = train_sampler
         Path(self.output_dir + '/model/').mkdir(exist_ok=True)
 
-    def unwrap_model(model):
-        return model.module if hasattr(model, "module") else model
+        if resume_path is None:
+            self.global_epoch = 0
+        else:
+            self.global_epoch = torch.load(resume_path,map_location=device)['global_epoch']
+
+        self.current_epoch = 0 + self.global_epoch
 
     def train_epoch(self):
         self.model.train()
@@ -36,7 +42,7 @@ class Trainer(object):
         all_logits = []
         all_labels = []
         all_index = []
-        for idx, data in enumerate(tqdm(self.train_dataloader)):
+        for idx, data in enumerate(tqdm(self.train_dataloader, disable=not self.is_main_process)):
             x, x_pos, x_pad, x_mask, aa_label = data[0].to(self.device), data[1].to(self.device), \
                 data[2].to(self.device), data[3].to(self.device), data[4].to(self.device)
             # print(x.shape)
@@ -62,10 +68,15 @@ class Trainer(object):
         all_logits = torch.cat(all_logits, dim=0)
         all_labels = torch.cat(all_labels, dim=0)
         all_index = torch.cat(all_index, dim=0)
-        print("Training metrics:")
+
+        if self.is_main_process:
+            print("Training metrics:")
         return self.log_metrics(all_labels, all_logits, all_index)
 
-    def save_model(self, save_epochs, mode='best'):
+    def save_model(self, save_epochs, curr_loss, mode='best'):
+        def unwrap_model(model):
+            return model.module if hasattr(model, "module") else model
+
         config_dict = OmegaConf.to_container(self.config, resolve=True)
         if mode == 'last':
             data = {
@@ -73,6 +84,9 @@ class Trainer(object):
                 'epoch': save_epochs,
                 'model': unwrap_model(self.model).state_dict(),
                 'opt': self.optimizer.state_dict(),
+                'scheduler':self.scheduler.state_dict() if self.scheduler else None,
+                'global_epoch': self.current_epoch,
+                'loss': curr_loss
             }
         elif mode == 'curr':
             data = {
@@ -80,6 +94,9 @@ class Trainer(object):
                 'epoch': save_epochs,
                 'model': unwrap_model(self.model).state_dict(),
                 'opt': self.optimizer.state_dict(),
+                'scheduler': self.scheduler.state_dict() if self.scheduler else None,
+                'global_epoch': self.current_epoch,
+                'loss': curr_loss
             }
         else:
             raise ValueError(f"unknown mode {mode}")
@@ -105,27 +122,37 @@ class Trainer(object):
         micro_replace_f1 = f1_score(labels[index == 2], pred_labels[index == 2], average='micro')
         micro_keep_f1 = f1_score(labels[index == 3], pred_labels[index == 3], average='micro')
 
-        print("Epoch: %d, all_loss: %f, mask_loss: %f, replace_loss: %f, keep_loss: %f" % (
-            self.current_epoch, all_loss.item(), mask_loss.item(), replace_loss.item(), keep_loss.item()))
-        print("Macro Mask F1: %f, Macro Replace F1: %f, Macro Keep F1: %f" % (
-            macro_mask_f1, macro_replace_f1, macro_keep_f1))
-        print("Micro Mask F1: %f, Micro Replace F1: %f, Micro Keep F1: %f" % (
-            micro_mask_f1, micro_replace_f1, micro_keep_f1))
+        if self.is_main_process:
+            print("Epoch: %d, all_loss: %f, mask_loss: %f, replace_loss: %f, keep_loss: %f" % (
+                self.current_epoch, all_loss.item(), mask_loss.item(), replace_loss.item(), keep_loss.item()))
+            print("Macro Mask F1: %f, Macro Replace F1: %f, Macro Keep F1: %f" % (
+                macro_mask_f1, macro_replace_f1, macro_keep_f1))
+            print("Micro Mask F1: %f, Micro Replace F1: %f, Micro Keep F1: %f" % (
+                micro_mask_f1, micro_replace_f1, micro_keep_f1))
 
         return all_loss, macro_mask_f1, macro_replace_f1, macro_keep_f1, micro_mask_f1, micro_replace_f1, micro_keep_f1
 
     def fit(self):
-        for epoch in range(self.train_num_epochs):
+        if self.current_epoch >= self.train_num_epochs and self.is_main_process:
+            print(f"Already reached target epoch {self.train_num_epochs}, current epoch is {self.current_epoch}.")
+            return
+
+        while self.current_epoch < self.train_num_epochs:
+            if self.train_sampler is not None:
+                self.train_sampler.set_epoch(self.current_epoch)
             train_loss, train_mac_mask_f1, train_mac_replace_f1, train_mac_keep_f1, train_mic_mask_f1, \
             train_mic_replace_f1, train_mic_keep_f1 = self.train_epoch()
-            if self.experiment:
+            if self.experiment and self.is_main_process:
                 self.experiment.log_metric('train_macro_mask_f1', train_mac_mask_f1, epoch=self.current_epoch)
                 self.experiment.log_metric('train_macro_replace_f1', train_mac_replace_f1, epoch=self.current_epoch)
                 self.experiment.log_metric('train_macro_keep_f1', train_mac_keep_f1, epoch=self.current_epoch)
                 self.experiment.log_metric('train_micro_mask_f1', train_mic_mask_f1, epoch=self.current_epoch)
                 self.experiment.log_metric('train_micro_replace_f1', train_mic_replace_f1, epoch=self.current_epoch)
                 self.experiment.log_metric('train_micro_keep_f1', train_mic_keep_f1, epoch=self.current_epoch)
-            if self.current_epoch % self.save_epochs == 0 and self.current_epoch > 10:
-                self.save_model(self.current_epoch, mode='curr')
+            if self.current_epoch % self.save_epochs == 0 and self.current_epoch > 10 and self.is_main_process:
+                self.save_model(self.current_epoch, train_loss, mode='curr')
+
             torch.cuda.empty_cache()
-        self.save_model(self.current_epoch, mode='last')
+
+        if self.is_main_process:
+            self.save_model(self.current_epoch, train_loss, mode='last')
